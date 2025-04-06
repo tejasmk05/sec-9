c1fe908bf5dd999f93c204ab1288c514a0d754f4
Tejas Gowda <tejasmkgowda@gmail.com>
2025-04-06 23:20:50 +0530

    JS change 3

--- .github/actions/javascript-actions/main.js
+++ .github/actions/javascript-actions/main.js
@@ -1,7 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec')
 
 function run(){
-    core.notice("My first javascript action");
+    //get input values
+    const bucket = core.getInput('bucket', { required: true })
+    const bucketRegion = core.getInput('bucketRegion', { required: true })
+    const bucketFolder = core.getInput('bucketFolder', { required: true })
+
+    //upload to S3 bucket
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 synch ${bucketFolder} ${s3Uri} --region ${bucketRegion}`)
+
 }
 run();
