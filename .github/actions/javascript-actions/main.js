6e75e2e7dd39bb1bc4c7ae7021a8f7f8c9a3ae0a
Tejas Gowda <tejasmkgowda@gmail.com>
2025-04-07 09:19:14 +0530

    JS change 1

--- .github/actions/javascript-actions/main.js
+++ .github/actions/javascript-actions/main.js
@@ -1,16 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const exec = require('@actions/exec')
+const exec = require('@actions/exec');
 
 function run(){
     //get input values
-    const bucket = core.getInput('bucket', { required: true })
-    const bucketRegion = core.getInput('bucketRegion', { required: true })
-    const bucketFolder = core.getInput('bucketFolder', { required: true })
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucketRegion', { required: true });
+    const bucketFolder = core.getInput('bucketFolder', { required: true });
 
     //upload to S3 bucket
-    const s3Uri = `s3://${bucket}`
-    exec.exec(`aws s3 sync ${bucketFolder} ${s3Uri} --region ${bucketRegion}`)
-
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${bucketFolder} ${s3Uri} --region ${bucketRegion}`);
+    
+    //Output the URL details
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion.amazonaws.com}`;
+    core.setOutput('website-url', websiteUrl);
 }
 run();
